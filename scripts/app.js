ee4d28468e1876da4deb4b7e0d81579101dbb3de
ikismail <ikismail7@gmail.com>
2017-08-28 21:05:27 +0530

    update

--- scripts/app.js
+++ scripts/app.js
@@ -2,17 +2,8 @@
  * @Author: Mohammed Ismail <ikismail7>
  * @Date:   2017-08-28T16:14:30+05:30
  * @Last modified by:   ikismail7
- * @Last modified time: 2017-08-28T19:15:23+05:30
+ * @Last modified time: 2017-08-28T20:51:49+05:30
  */
-var app = angular.module('myApp', ["ngRoute"]);
 
-app.config(function($routeProvider) {
-  $routeProvider
-  .when("/default", {
-    templateUrl : "view/default.html"
-  })
-  .otherwise({
-       template : "<h1>None</h1><p>Nothing has been selected</p>"
-   });
 
-});
+
--- scripts/appService.js
+++ scripts/appService.js
@@ -2,5 +2,8 @@
  * @Author: Mohammed Ismail <ikismail7>
  * @Date:   2017-08-28T19:00:36+05:30
  * @Last modified by:   ikismail7
- * @Last modified time: 2017-08-28T19:08:19+05:30
+ * @Last modified time: 2017-08-28T20:51:48+05:30
  */
+
+
+
