d43ad2c4e146781c01404717061f7e8f777ec13d
ikismail <ikismail7@gmail.com>
2017-08-28 19:24:09 +0530

    Implemented AngularJs and route functions

---
+++ scripts/appController.js
@@ -0,0 +1,11 @@
+/**
+ * @Author: Mohammed Ismail <ikismail7>
+ * @Date:   2017-08-28T16:31:35+05:30
+ * @Last modified by:   ikismail7
+ * @Last modified time: 2017-08-28T19:03:02+05:30
+ */
+ app.controller('appController', ['$scope', '$http', function($scope,
+   $http) {
+
+      $scope.data = "Hello World"
+   }])
---
+++ scripts/appService.js
@@ -0,0 +1,6 @@
+/**
+ * @Author: Mohammed Ismail <ikismail7>
+ * @Date:   2017-08-28T19:00:36+05:30
+ * @Last modified by:   ikismail7
+ * @Last modified time: 2017-08-28T19:08:19+05:30
+ */
