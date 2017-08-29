c98da893dff11e7d71773cf046c347c980824ed7
ikismail <ikismail7@gmail.com>
2017-08-29 14:39:44 +0530

    Updated page

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,21 @@
-  /**
-   * @Author: Mohammed Ismail <ikismail7>
-   * @Date:   2017-08-28T16:14:30+05:30
+/**
+* @Author: Mohammed Ismail <ikismail7>
+* @Date:   2017-08-28T16:14:30+05:30
  * @Last modified by:   ikismail7
- * @Last modified time: 2017-08-29T10:44:46+05:30
-   */
-  var app = angular.module('myApp', ["ngRoute"]);
-   app.config(function($routeProvider) {
-     $routeProvider
-     .when("/default", {
-       templateUrl : "view/businessPage.html"
-     })
-      .otherwise({
-          template : "<h1>None</h1><p>Nothing has been selected</p>"
-      });
-   });
+ * @Last modified time: 2017-08-29T12:49:54+05:30
+*/
+var app = angular.module('myApp', ["ngRoute"]);
+app.config(function($routeProvider) {
+  $routeProvider
+  .when("/", {
+    templateUrl : "view/mainPage.html"
+  })
+  .when("/home", {
+    templateUrl : "view/mainPage.html"
+  })
+  .when("/category", {
+    templateUrl : "view/categoryPage.html"
+  }).otherwise({
+    redirectTo : '/'
+  });
+});
