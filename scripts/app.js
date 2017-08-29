adcdde0121eb0c12becce77c92809f03f015a10e
ikismail <ikismail7@gmail.com>
2017-08-29 14:46:22 +0530

    Merge remote-tracking branch 'origin/development' into development

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,7 @@
 * @Author: Mohammed Ismail <ikismail7>
 * @Date:   2017-08-28T16:14:30+05:30
  * @Last modified by:   ikismail7
- * @Last modified time: 2017-08-29T14:40:34+05:30
+ * @Last modified time: 2017-08-29T14:42:42+05:30
 */
 var app = angular.module('myApp', ["ngRoute"]);
 app.config(function($routeProvider) {
@@ -14,7 +14,7 @@ app.config(function($routeProvider) {
     templateUrl : "view/mainPage.html"
   })
   .when("/category", {
-    templateUrl : "view/categoryPage.html"
+    templateUrl : "view/newsPage.html"
   }).otherwise({
     redirectTo : '/'
   });
