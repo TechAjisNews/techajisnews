d440ebd9b6ab4ba3575c285defa2228f540ca723
ikismail <ikismail7@gmail.com>
2017-08-31 10:01:19 +0530

    Updated changes

--- scripts/app.js
+++ scripts/app.js
@@ -1,29 +1,30 @@
 /**
-* @Author: Mohammed Ismail <ikismail7>
-* @Date:   2017-08-28T16:14:30+05:30
- * @Last modified by:   ikismail7
- * @Last modified time: 2017-08-29T21:44:52+05:30
-*/
-var app = angular.module('myApp', ["ngRoute","angular-loading-bar"]);
-app.config(function($routeProvider) {
-  $routeProvider
-  .when("/", {
-    templateUrl : "view/mainPage.html"
   })
-  .when("/home", {
-    templateUrl : "view/mainPage.html"
-  })
-  .when("/category", {
-    templateUrl : "view/newsPage.html"
-  })
-  .when("/newspapers", {
-    templateUrl : "view/allNewsPapers.html"
-  })
-  .otherwise({
-    redirectTo : '/'
-  });
-});
+ * @Author: Mohammed Ismail
+ * @Date:   2017-08-30T16:05:45+05:30
+ * @Last modified by:   Mohammed Ismail
+ * @Last modified time: 2017-08-31T09:58:50+05:30
+ */
+ var app = angular.module('myApp', ["ngRoute","angular-loading-bar"]);
+ app.config(function($routeProvider) {
+   $routeProvider
+   .when("/", {
+     templateUrl : "view/mainPage.html"
+   })
+   .when("/home", {
+     templateUrl : "view/mainPage.html"
+   })
+   .when("/category", {
+     templateUrl : "view/newsPage.html"
+   })
+   .when("/newspapers", {
+     templateUrl : "view/allNewsPapers.html"
+   })
+   .otherwise({
+     redirectTo : '/'
+   });
+ });
 
-app.config(['cfpLoadingBarProvider', function(cfpLoadingBarProvider) {
-    cfpLoadingBarProvider.parentSelector = '#loading-bar-container';
-  }])
+ app.config(['cfpLoadingBarProvider', function(cfpLoadingBarProvider) {
+     cfpLoadingBarProvider.parentSelector = '#loading-bar-container';
+ }])
--- scripts/appController.js
+++ scripts/appController.js
@@ -1,43 +1,43 @@
 /**
-* @Author: Mohammed Ismail <ikismail7>
-* @Date:   2017-08-28T16:31:35+05:30
- * @Last modified by:   ikismail7
- * @Last modified time: 2017-08-30T13:10:15+05:30
-*/
-app.controller('newsController', ['$scope','appService',function(
-  $scope,appService) {
+ * @Author: Mohammed Ismail
+ * @Date:   2017-08-30T16:05:45+05:30
+ * @Last modified by:   Mohammed Ismail
+ * @Last modified time: 2017-08-31T09:59:09+05:30
+ */
+ app.controller('newsController', ['$scope','appService',function(
+   $scope,appService) {
 
-    $scope.category;
-    $scope.sourceData = [];
-    $scope.image ;
+     $scope.category;
+     $scope.sourceData = [];
+     $scope.image ;
 
-    $scope.getNewsBySource = function(category){
-      $scope.category = category;
-      appService.getNewsBySource(category).then(function(data){
-        $scope.sourceData = data;
-      },function(error){
-        // $.toaster({ priority : 'error', title : 'Error', message : 'error while fetching resources'});
-      })
-    };
-     $scope.getNewsByLanguage = function(language){
-      console.log('language ->',language);
-      appService.getNewsByLanguage(language).then(function(data){
-        $scope.sourceData = data;
-        console.log($scope.sourceData)
-      },function(error){
-        // $.toaster({ priority : 'error', title : 'Error', message : 'error while fetching resources'});
-      })
-    };
+     $scope.getNewsBySource = function(category){
+       $scope.category = category;
+       appService.getNewsBySource(category).then(function(data){
+         $scope.sourceData = data;
+       },function(error){
+         // $.toaster({ priority : 'error', title : 'Error', message : 'error while fetching resources'});
+       })
+     };
+      $scope.getNewsByLanguage = function(language){
+       console.log('language ->',language);
+       appService.getNewsByLanguage(language).then(function(data){
+         $scope.sourceData = data;
+         console.log($scope.sourceData)
+       },function(error){
+         // $.toaster({ priority : 'error', title : 'Error', message : 'error while fetching resources'});
+       })
+     };
 
-    $scope.getAllSources = function(){
-      console.log('RSS-News7');
-      appService.getAllSources().then(function(data){
-        $scope.sources = data;
-      },function(errorResponse){
-        // $.toaster({ priority : 'error', title : 'Error', message : 'error while fetching resources'});
-      })
-    }
+     $scope.getAllSources = function(){
+       console.log('RSS-News7');
+       appService.getAllSources().then(function(data){
+         $scope.sources = data;
+       },function(errorResponse){
+         // $.toaster({ priority : 'error', title : 'Error', message : 'error while fetching resources'});
+       })
+     }
 
-    $scope.getAllSources();
+     $scope.getAllSources();
 
-  }])
+ }])
