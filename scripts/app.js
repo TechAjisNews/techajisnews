31ac4d2fa3882e19cd8c844a86ca62bae54919da
ikismail <ikismail7@gmail.com>
2017-08-31 12:02:22 +0530

    completed allNewspaper section

--- scripts/app.js
+++ scripts/app.js
@@ -3,22 +3,22 @@
  * @Author: Mohammed Ismail
  * @Date:   2017-08-30T16:05:45+05:30
  * @Last modified by:   Mohammed Ismail
- * @Last modified time: 2017-08-31T09:58:50+05:30
+ * @Last modified time: 2017-08-31T11:24:29+05:30
  */
  var app = angular.module('myApp', ["ngRoute","angular-loading-bar"]);
  app.config(function($routeProvider) {
    $routeProvider
    .when("/", {
-     templateUrl : "view/mainPage.html"
+     templateUrl : "view/category.html"
    })
    .when("/home", {
-     templateUrl : "view/mainPage.html"
+     templateUrl : "view/category.html"
    })
    .when("/category", {
-     templateUrl : "view/newsPage.html"
+     templateUrl : "view/category.html"
    })
    .when("/newspapers", {
-     templateUrl : "view/allNewsPapers.html"
+     templateUrl : "view/allNewsPaper.html"
    })
    .otherwise({
      redirectTo : '/'
