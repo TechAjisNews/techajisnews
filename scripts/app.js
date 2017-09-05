faa1342937fc280a01e3c85c79653306f4ceff3b
ikismail <ikismail7@gmail.com>
2017-09-05 15:49:24 +0530

    Updated changes

--- scripts/app.js
+++ scripts/app.js
@@ -2,13 +2,13 @@
  * @Author: Mohammed Ismail
  * @Date:   2017-08-30T16:05:45+05:30
  * @Last modified by:   Mohammed Ismail
- * @Last modified time: 2017-09-05T10:58:13+05:30
+ * @Last modified time: 2017-09-05T15:38:58+05:30
  */
  var app = angular.module('myApp', ["ngRoute","angular-loading-bar"]);
  app.config(function($routeProvider) {
    $routeProvider
    .when("/", {
-     templateUrl : "view/category.html"
+     templateUrl : "view/mainPage.html"
    })
    .when("/home", {
      templateUrl : "view/mainPage.html"
@@ -21,6 +21,9 @@
    })
    .when("/topstory", {
      templateUrl : "view/topstories.html"
+   })
+   .when("/contact", {
+     templateUrl : "view/contact.html"
    })
    .otherwise({
      redirectTo : '/'
