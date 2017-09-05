b91326353931233fdea471540cdcb10bcc8c1488
ikismail <ikismail7@gmail.com>
2017-09-05 11:23:50 +0530

    updataion

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,8 @@
 /**
-  })
  * @Author: Mohammed Ismail
  * @Date:   2017-08-30T16:05:45+05:30
  * @Last modified by:   Mohammed Ismail
- * @Last modified time: 2017-08-31T15:14:56+05:30
+ * @Last modified time: 2017-09-05T10:58:13+05:30
  */
  var app = angular.module('myApp', ["ngRoute","angular-loading-bar"]);
  app.config(function($routeProvider) {
