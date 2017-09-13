4c89ef078a7a3f6e12f2d6fe55281cbd53b24a43
ikismail <ikismail7@gmail.com>
2017-09-13 18:19:20 +0530

    Implemented Indian feed of puthiyathalaimurai

--- scripts/appService.js
+++ scripts/appService.js
@@ -15,6 +15,7 @@ app.factory('appService', ['$http', function ($http) {
     var GET_STORIES = "https://newsapi.org/v1/articles?source=";
     var GET_RSSFEEDS = "https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Ffeeds.feedburner.com%2FPuthiyathalaimurai_Districts_News";
     var RSSFEED_PT = "https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Ffeeds.feedburner.com%2FPuthiyathalaimurai_Tamilnadu_News&api_key=3159ncurciuuew1nmfhqt5zap8r7nqisqzcx4yvt&order_by=pubDate&order_dir=desc&count=50"
+    var RSSFEED_IndiaNews_PT = " https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Ffeeds.feedburner.com%2FPuthiyathalaimurai_India_News%3Fformat%3Dxmlhttps%3A%2F%2Fnews.ycombinator.com%2Frss&api_key=3159ncurciuuew1nmfhqt5zap8r7nqisqzcx4yvt&order_by=pubDate&order_dir=asc&count=50";
     var generatedValue = "10ad575c68f24879949f89147d38c9ce";
 
     appService.getNewsByCategory = function (category) {
@@ -62,6 +63,37 @@ app.factory('appService', ['$http', function ($http) {
 
     appService.getLocalRssFeeds = function () {
         return $http.get(RSSFEED_PT).then(function (response) {
+
+          console.log('localNews service', response.data);
+
+                for (var i = 0; i < response.data.items.length; i++) {
+                    var item = response.data.items[i];
+                    var img = item.content.match(/http:.*?.jpg/g);
+                    var content = item.content.match(/<p>.*?<\/p\>/g);
+                    content.shift();
+                    content.pop();
+
+                    var content_modified = {
+                      "title" : $(content[0]).text(),
+                      "body" : $(content[1]).text(),
+                      "description" : $(content[2]).text(),
+                      "image_url" : img[0]
+                    }
+
+                    response.data.items[i]['content_modified'] = content_modified;
+                }
+
+                return response.data;
+            }, function (errorResponse) {
+                console.log('Error while fetching all sources');
+            })
+    };
+
+    appService.getRSSIndianNews = function () {
+        return $http.get(RSSFEED_IndiaNews_PT).then(function (response) {
+
+          console.log('localNews service', response.data);
+
                 for (var i = 0; i < response.data.items.length; i++) {
                     var item = response.data.items[i];
                     var img = item.content.match(/http:.*?.jpg/g);
